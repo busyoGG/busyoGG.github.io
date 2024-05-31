6522b57ebb54a86ce71fd1b1dd0a9ae64ba99467
BUSYO\buxiu <694253220@qq.com>
2024-05-31 20:05:28 +0800

    Site updated: 2024-05-31 20:05:07

--- script/AfterProcess.js
+++ script/AfterProcess.js
@@ -132,7 +132,7 @@ window.AfterProcess = {
     AfterProcess.ResetCodeStyle();
     AfterProcess.ResetTitle();
     AfterProcess.InitVideo();
-    mermaid.init(undefined, '.mermaid');
+    mermaid.init(undefined, ".mermaid");
   },
 };
 AfterProcess.Init();
